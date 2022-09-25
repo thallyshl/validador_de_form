bc2de756b35daad92b3cd4f189df6186797e0935
Antonio <antoniodias1106@gmail.com>
2022-09-25 15:58:53 -0300

    Criação da estrutura básica de cadastro com HTML e CSS e verificando se tem dado nos input com JS

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,17 @@
-alert('Deu certo')
+const btn = document.querySelector(".btn")
+const email = document.querySelector(".email").value;
+const password = document.querySelector(".password").value;
+
+btn.addEventListener('click', cadastrar)
+
+
+
+function cadastrar() {
+    if (email.value === "" || password.value === "") {
+        alert("Coloque todos os dados")
+    } else {
+        alert("Deu certo")
+    }
+
+}
+
