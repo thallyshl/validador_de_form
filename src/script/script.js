07af6d1c0e321b42a73e4587ba12bc6f1bb6a0fd
Antonio <antoniodias1106@gmail.com>
2022-09-28 20:42:49 -0300

    Ajeitei algumas coisas na semantica, adicionei sombras nos inputs e no button e dei inicio ao script js

--- src/script/script.js
+++ src/script/script.js
@@ -1,17 +1,48 @@
-const btn = document.querySelector(".btn")
-const email = document.querySelector(".email").value;
-const password = document.querySelector(".password").value;
+const validator = {
+    handleSubmit:(event)=>{
+        event.preventDefault();
 
-btn.addEventListener('click', cadastrar)
+        const send = true
+        const inputs = sign.querySelectorAll('input')
 
+        for(let i = 0; i<inputs.length; i++){
+            let input = inputs [i]
+            let check = validator.checkInput(input)
+            if(check !== true){
+                send = false
+                console.log(check)
+            }
+        }
 
+        if(send){
+            sign.submit();
+        }
+    },
+    checkInput:(input) =>{
+        let rules = input.getAttribute('data-rules')
+        if(rules !== null){
+            rules = rules.split('|');
+            for(let k in rules){
+                let rDetails = rules[k].split('=')
+                switch(rDetails[0]){
+                    case 'required':
+                        if(input.value !== ""){
+                            return "Campo não pode ser vazio"
+                        }
+                    break;
+                    
+                }
+            }
+        }
+        return true
 
-function cadastrar() {
-    if (email.value === "" || password.value === "") {
-        alert("Coloque todos os dados")
-    } else {
-        alert("Deu certo")
     }
-
 }
+
+const form = document.querySelector(".form")
+
+form.addEventListener("submit", validator.handleSubmit);
+
+
+
 
