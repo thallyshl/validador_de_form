7f5bae569498b460c5b8d7902f2dda3e933cd9e3
Antonio <antoniodias1106@gmail.com>
2022-10-03 23:21:32 -0300

    regex da senha

--- src/script/script.js
+++ src/script/script.js
@@ -9,14 +9,23 @@ const validator = () => {
     const email = document.querySelector('.email');
     const password = document.querySelector('.password');
     const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+    const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/
 
     if (emailRegex.exec(email.value)) {
-        console.log('igual');
+        ;
     } else {
         const small = document.querySelector('.erroEmail');
         small.className = 'error';
         small.innerHTML =  'Email inválido';
 
+    }
+    if (passwordRegex.exec(password.value)){
+        const small = document.querySelector('.erroPassword');
+        
+    }else{
+        const small = document.querySelector('.erroPassword');
+        small.className = 'error';
+        small.innerHTML =  'Sua senha deve conter 8 caracteres sendo pelo menos um</br> deles maiúsculo, um número e um caractere especial';
     }
 
 
